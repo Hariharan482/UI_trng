8315de70cffe9918e253370eedf964113c8b26e3
Hariharan482 <125631751+Hariharan482@users.noreply.github.com>
2023-03-24 16:18:16 +0530

    Exercise15

---
+++ blender movies/index.js
@@ -0,0 +1,42 @@
+fetch('/video.json')
+    .then(data=>data.json())
+    .then((res)=>{
+        let videoSect=document.querySelector(".video-section");
+        var video=document.createElement("video");
+        video.src=res.videoUrl;
+        video.controls=true;
+        video.poster="/images/Screenshot 2023-03-22 at 7.25.34 PM.png";
+        videoSect.appendChild(video);
+        var h2=document.createElement("h2");
+        h2.innerText=res.title;
+        videoSect.after(h2);
+        var p=document.createElement("p");
+        p.setAttribute("class","video-descrptn");
+        p.innerText=res.description;
+        document.querySelector("h2").after(p);
+        let hr=document.createElement("hr");
+        p.after(hr);
+        let h3=document.createElement("h3");
+        h3.innerText="Comments";
+        hr.after(h3);
+
+
+
+        
+        hr1.setAttribute("class","hr-tag");
+        document.querySelector(".left-container").appendChild(hr1);
+    })
+
+fetch('/posters.json')
+    .then(data=>data.json())
+    .then((res)=>{
+        for(let image of res){
+            let div=document.createElement('div');
+            div.setAttribute("class","imag-container");
+            let img=document.createElement("img");
+            img.src=image.imageUrl;
+            img.alt=image.title;
+            div.appendChild(img);
+            document.querySelector(".right-container").append(div);
+        }
+    })
