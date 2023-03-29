0e7983a55d5e76de5e97c28042d8bb3404eac15a
Hariharan482 <125631751+Hariharan482@users.noreply.github.com>
2023-03-29 23:56:09 +0530

    changes done

--- Ex17/index.js
+++ Ex17/index.js
@@ -1,36 +1,22 @@
 $(document).ready(function(){
-  
-  $section = $('.content-section').children();
-  $tab=$('.section-tabs').children();
 
   $( function() {
     $( "#accordion" ).accordion();
   } );
 
+  $( function() {
+    $( "#tabs" ).tabs();
+  } );
   
-  var prevTab=$('.active');
-  var prevSect=$('.about-us-section');
-
-  $('.tab').click(function(e){
-    prevTab.toggleClass('active');
-    $(this).toggleClass('active');
-    prevTab=$(this);
-
-    $cName="."+$(this)[0].className.split(" ")[1]+"-section";
-    // console.log($cName);
-    prevSect.toggleClass('tab-display');
-    $($cName).toggleClass('tab-display');
-    prevSect=$($cName);
-  })
-
+  
   $.getJSON("/assets/locations.json",function(locations){
     for(let i=0;i<locations.length;i++){
       var $container=$('<div>');
       if(i%2==0){
-        $('.locations-section').append($($container).addClass("locations-container grey-container"));
+        $('.tab3').append($($container).addClass("locations-container grey-container"));
       }
       else {
-        $('.locations-section').append($($container).addClass("locations-container pink-container"));
+        $('.tab3').append($($container).addClass("locations-container pink-container"));
       }
       if(locations[i].state=="Tamil Nadu"){
         var $img=$('<img>').attr("src","/assets/India.png");
