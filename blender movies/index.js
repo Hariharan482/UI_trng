007883e32f20c4e3da0475c96a1e0e9bf927e7c0
Hariharan482 <125631751+Hariharan482@users.noreply.github.com>
2023-03-29 10:27:19 +0530

    updatwd

--- blender movies/index.js
+++ blender movies/index.js
@@ -19,10 +19,29 @@ fetch('/video.json')
         let h3=document.createElement("h3");
         h3.innerText="Comments";
         hr.after(h3);
-
-
-
-        
+        for(let comment of res.comments){
+            let card=document.createElement("div");
+            card.setAttribute("class","card");
+            let cardimg=document.createElement("div");
+            cardimg.setAttribute("class","cardimg");
+            let img=document.createElement('img');
+            img.setAttribute("src",comment.image);
+            cardimg.append(img);
+            let text=document.createElement("div");
+            text.setAttribute("class","text");
+            let p=document.createElement('p');
+            p.setAttribute("class","name");
+            p.innerHTML=comment.name;
+            let p1=document.createElement("p");
+            p1.setAttribute('class',"comment");
+            p1.innerHTML=comment.comment;
+            text.append(p);
+            text.append(p1);
+            card.append(cardimg);
+            card.append(text);
+            document.querySelector(".comment-section").append(card);
+        }
+        let hr1=document.createElement("hr");
         hr1.setAttribute("class","hr-tag");
         document.querySelector(".left-container").appendChild(hr1);
     })
