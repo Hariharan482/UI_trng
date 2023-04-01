a664876a07184a443cf9977a03ff027e55d75946
Hariharan482 <125631751+Hariharan482@users.noreply.github.com>
2023-04-01 06:51:44 +0530

    Chnages done

--- blender movies/index.js
+++ blender movies/index.js
@@ -1,61 +1,76 @@
 fetch('/video.json')
-    .then(data=>data.json())
-    .then((res)=>{
-        let videoSect=document.querySelector(".video-section");
-        var video=document.createElement("video");
-        video.src=res.videoUrl;
+    .then(data => data.json())
+    .then((res) => {
+        let videoSect = document.querySelector(".video-section");
+        let video = createElement('video','video-player');
+        video.src = res.videoUrl;
         video.controls=true;
-        video.poster="/images/Screenshot 2023-03-22 at 7.25.34 PM.png";
+        video.poster = "/images/Screenshot 2023-03-22 at 7.25.34 PM.png";
+        video.alt-"poster";
         videoSect.appendChild(video);
-        var h2=document.createElement("h2");
-        h2.innerText=res.title;
+        let h2 = createElement('h2','h2');
+        h2.innerText = res.title;
         videoSect.after(h2);
-        var p=document.createElement("p");
-        p.setAttribute("class","video-descrptn");
-        p.innerText=res.description;
+        let p=createElement('p','video-descrptn');
+        p.innerText = res.description;
         document.querySelector("h2").after(p);
-        let hr=document.createElement("hr");
+        let hr=createElement('hr','break-line');
         p.after(hr);
-        let h3=document.createElement("h3");
-        h3.innerText="Comments";
+        let h3=createElement('h3','comments')
+        h3.innerText = "Comments";
         hr.after(h3);
-        for(let comment of res.comments){
-            let card=document.createElement("div");
-            card.setAttribute("class","card");
-            let cardimg=document.createElement("div");
-            cardimg.setAttribute("class","cardimg");
-            let img=document.createElement('img');
-            img.setAttribute("src",comment.image);
+        for (let comment of res.comments) {
+            let card=createElement('div','card');
+            let cardimg = createElement("div","cardimg");
+            let img = createElement('img');
+            img.setAttribute("src", comment.image);
+            img.alt="cardimg";
             cardimg.append(img);
-            let text=document.createElement("div");
-            text.setAttribute("class","text");
-            let p=document.createElement('p');
-            p.setAttribute("class","name");
-            p.innerHTML=comment.name;
-            let p1=document.createElement("p");
-            p1.setAttribute('class',"comment");
-            p1.innerHTML=comment.comment;
+            let text=createElement('div','text');
+            let p=createElement('p','name');
+            p.innerHTML = comment.name;
+            let p1=createElement('p','comment');
+            p1.innerHTML = comment.comment;
             text.append(p);
             text.append(p1);
             card.append(cardimg);
             card.append(text);
             document.querySelector(".comment-section").append(card);
         }
-        let hr1=document.createElement("hr");
-        hr1.setAttribute("class","hr-tag");
+        let hr1=createElement('hr','hr-tag');
         document.querySelector(".left-container").appendChild(hr1);
+    }).catch((err) => {
+        console.log(err);
     })
 
 fetch('/posters.json')
-    .then(data=>data.json())
-    .then((res)=>{
-        for(let image of res){
-            let div=document.createElement('div');
-            div.setAttribute("class","imag-container");
-            let img=document.createElement("img");
-            img.src=image.imageUrl;
-            img.alt=image.title;
+    .then(data => data.json())
+    .then((res) => {
+        for (let image of res) {
+            let div=createElement('div','imag-container')
+            // let div = document.createElement('div');
+            // div.setAttribute("class", "imag-container");
+            let img = createElement("img",'image');
+            img.src = image.imageUrl;
+            img.alt = image.title;
             div.appendChild(img);
             document.querySelector(".right-container").append(div);
         }
+
+    }).catch((err) => {
+        console.log(err);
     })
+
+function createElement(elementType,className){
+    let element=document.createElement(elementType);
+    element.setAttribute('class',className);
+    return element;
+} 
+        
+document.addEventListener('DOMContentLoaded', () => {
+    let videosect= document.querySelector('.video-section').children;
+    document.querySelector('.video-section').addEventListener('click',()=>{
+        console.log(videosect);
+        videosect.setAttribute('controls','');
+    })
+})
